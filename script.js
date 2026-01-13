fc1d937c142fa51e508aadad85b6415f4bc47d94
AdityaVerma <adityaverma8392@gmail.com>
2026-01-13 23:04:29 +0530

    script.js modified

---
+++ script.js
@@ -0,0 +1,13 @@
+var name = "Aditya Verma";
+console.log(this.name);
+
+
+var addition = (a, b, c) => {
+    return a + b + c;
+}
+var sum = addition(1, 2, 3);
+console.log(sum);
+
+
+console.log(alert("I'm trying to learn javaScript"));
+
