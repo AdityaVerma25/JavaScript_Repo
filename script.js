38e3416fdf2dfaa6168cd66140e11a1c314604c9
AdityaVerma <adityaverma8392@gmail.com>
2026-01-13 23:13:56 +0530

    some new change applied in index.html and script.js

--- script.js
+++ script.js
@@ -2,12 +2,8 @@ var name = "Aditya Verma";
 console.log(this.name);
 
 
-var addition = (a, b, c) => {
-    return a + b + c;
-}
-var sum = addition(1, 2, 3);
-console.log(sum);
-
-
-console.log(alert("I'm trying to learn javaScript"));
-
+var func = () => { alert("Click is working"); }
+document.addEventListener("click",()=> {
+   func();
+    console.log("working");
+})
